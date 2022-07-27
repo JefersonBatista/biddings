719170ebb9fbda58ea4b89a176833362db69a28f
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-07-27 20:29:41 -0300

    List bidding process numbers

--- src/main/java/com/github/jb/biddings/controllers/BiddingController.java
+++ src/main/java/com/github/jb/biddings/controllers/BiddingController.java
@@ -1,8 +1,11 @@
 package com.github.jb.biddings.controllers;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +15,22 @@ public class BiddingController {
   public String showBiddings() {
     String url = "https://www.siga.es.gov.br/sgc/faces/pub/sgc/central/EditalPageList.jsp";
 
-    String html;
+    Document doc;
     try {
-      html = Jsoup.connect(url).get().html();
+      doc = Jsoup.connect(url).get();
     } catch (IOException e) {
       return "Site is down!";
     }
-    return html;
+
+    String biddingTableId = "form_EditalPageList:listaDataTable:tbody_element";
+    Element biddingTable = doc.getElementById(biddingTableId);
+
+    String processNumbers = "";
+    for (int i = 0; i < 10; i++) {
+      String processNumberIdFormat = "form_EditalPageList:listaDataTable:%d:lbNumeroProcesso";
+      String processNumberId = String.format(processNumberIdFormat, i);
+      processNumbers += biddingTable.getElementById(processNumberId).text() + "\n";
+    }
+    return processNumbers;
   }
 }
